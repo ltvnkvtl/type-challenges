902b8125fcab2a10d4a9fdad776a24dc90ee805c
vlitovkin <vlitovkin@intermedia.com>
2023-09-12 14:57:30 +0400

    medium: append arguments

--- playground/medium/00191-medium-append-argument.ts
+++ playground/medium/00191-medium-append-argument.ts
@@ -23,7 +23,11 @@
 
 /* _____________ Your Code Here _____________ */
 
-type AppendArgument<Fn, A> = any
+// type AppendArgument<Fn extends (...args: any[]) => unknown, A> =
+//     Fn extends (...args: infer Args) => infer Return ? (x: A, ...args: Args) => Return : never
+
+type AppendArgument<Fn extends (...args: any[]) => unknown, A> =
+    Fn extends (...args: infer Args) => infer Return ? (...args: [...Args, A]) => Return : never
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
