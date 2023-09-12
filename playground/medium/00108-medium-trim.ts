b0caf337c01ededb0ed188364be8728740a4138f
vlitovkin <vlitovkin@intermedia.com>
2023-09-12 12:22:21 +0400

    medium: trim

--- playground/medium/00108-medium-trim.ts
+++ playground/medium/00108-medium-trim.ts
@@ -17,8 +17,14 @@
 */
 
 /* _____________ Your Code Here _____________ */
+type Space = ' ' | '\n' | '\t'
 
-type Trim<S extends string> = any
+// type TrimLeft<S extends string> = S extends `${Space}${infer R}` ? TrimLeft<R> : TrimRight<S>
+// type TrimRight<S extends string> = S extends `${infer R}${Space}` ? TrimRight<R> : S
+// type Trim<S extends string> = S extends `${Space}${infer R}` ? TrimLeft<R> : TrimRight<S>
+// type Trim<S extends string> = TrimLeft<TrimRight<S>>
+
+type Trim<S extends string> = S extends `${Space}${infer R}` | `${infer R}${Space}` ? Trim<R> : S
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
